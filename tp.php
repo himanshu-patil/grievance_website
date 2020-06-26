d3e71fbc3577611186568b8461a80bc0a134802f
Himanshu patil <himanshu15122000@gmail.com>
2020-06-26 21:49:35 +0530

    student login issue solved

--- tp.php
+++ tp.php
@@ -1,33 +1,31 @@
 <?php
-require './connect.php ';
 
-$error=''; 
+require './connect.php' ;
 
-if(isset($_POST['submit'])){
 
-    if(empty($_POST['student id']) || empty($_POST['password'])){
-    $error = "Username or Password is Invalid";
-    }
-    else
-    {
-        
-        $user=$_POST['student id'];
-        $pass=$_POST['pass'];
-        
-         $conn = mysqli_connect("localhost", "root", "");////////////////////doubt
-        
-        $db = mysqli_select_db($conn, "test");
-        
-        $query = mysqli_query($conn, "SELECT * FROM user,pass WHERE password='$password' AND user='$student id'");
-        $rows = mysqli_num_rows($query);
-        if($rows == 1){
-            header("Location: ./sdashboard.html "); 
-        }
-        else
-        {
-            $error = "Username of Password is Invalid";
-        }
-        mysqli_close($conn); 
-    }
+$username=$_POST['name'];
+$password=$_POST['password'];
+
+// echo $username."  ".$password;
+
+$sql="SELECT `username`,`password` FROM teacher_db WHERE `username`='".$username."'and `password`='".$password."'limit 1";
+
+$result=mysqli_query($connection,$sql);
+
+
+
+if(empty($username) || empty($password))
+{
+    header('location : teacher_login.html');
 }
+elseif (mysqli_num_rows($result)==1)
+{
+    header('location:cdashboard.html');
+}
+else
+{
+    echo "invalid credebtials";
+    exit();
+}
+
 ?>
