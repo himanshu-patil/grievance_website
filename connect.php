2039c83544ab8db5add244c755a0b44e87e55e10
Arshad Majgaonkar <arshadmaj608@gmail.com>
2020-06-26 17:26:08 +0530

    teacher login php scripts

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+
+define('DB_HOST','localhost');
+define('DB_NAME','college_db');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','root');
+
+$connection=mysqli_connect(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME) or die("cannot connect");
+
+require 'connect.php' ;
+
+?>
---
+++ teacher_login.php
@@ -0,0 +1,27 @@
+<?php
+
+require './connect.php' ;
+
+$username=$_POST['name'];
+$password=$_POST['password'];
+
+$sql="SELECT * FROM teacher_db WHERE 'username'=".$username."and 'password'=".$password."limit 1";
+
+$result=mysqli_query($connection,$sql);
+
+
+if(empty($username) || empty($password))
+{
+    header('location : teacher_login.html');
+}
+elseif (mysqli_num_rows($result)==1)
+{
+    header('location:cdashboard.html');
+}
+else
+{
+    echo "invalid credebtials";
+    exit();
+}
+
+?>
