3a99bec6fdecafa9b8c0d228cf199043a0b01c34
Arshad Majgaonkar <arshadmaj608@gmail.com>
2020-06-27 13:26:02 +0530

    profile page updated

--- new_complain.php
+++ new_complain.php
@@ -9,5 +9,7 @@ $issue=$_POST['issue'];
 
 echo $subject."<br><br/>".$issue;
 
+$sql="INSERT INTO `complain_db`(`sr no`, `student id`, `date`, `subject`, `issue`, `status`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6])"
+
 
 ?>
