3d24937a0a1007c64044d98a6680a952acad7ffc
Arshad Majgaonkar <arshadmaj608@gmail.com>
2020-07-02 19:19:43 +0530

    bg updated

--- registered_complains.php
+++ registered_complains.php
@@ -17,6 +17,8 @@ if(empty($_SESSION['username']))
      <!-- Required meta tags -->
      <meta charset="utf-8">
      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+     
+     <link rel="stylesheet" type="text/css" href="./css/registered_complains.css">
      <!-- Bootstrap CSS -->
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
      <link rel="stylesheet" href="//cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
